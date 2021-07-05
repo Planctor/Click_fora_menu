422109ec0ea56b49d11599636f2856d9d671ca2e
Lucas Fernandes da Silva <47428909+Planctor@users.noreply.github.com>
2021-07-05 15:37:41 -0300

    Add files via upload

---
+++ classes/menu_mobile.js
@@ -0,0 +1,44 @@
+export default class Menu {
+    constructor(menu) {
+        this.menu = document.querySelector(menu)
+        this.itensMenu = [...this.menu.children]
+    }
+
+    cliqueFora() {
+        const html = document.documentElement
+        html.addEventListener('click', (e) => {
+            e.preventDefault();
+            if (this.itensMenu[0] !== e.target && this.itensMenu[2] !== e.target && this.itensMenu[1] !== e.target) {
+                this.itensMenu.forEach(iten => {
+                    iten.classList.remove('ativo');
+                });
+            }
+        })
+    }
+
+    adicionarCadaClasse() {
+        this.itensMenu.forEach(iten => {
+            iten.classList.add('ativo');
+        });
+
+        this.cliqueFora();
+    }
+
+    adicionarClasseEvento() {
+        this.itensMenu.forEach(iten => {
+            iten.addEventListener('click', this.adicionarCadaClasse)
+            iten.addEventListener('touchstart', this.adicionarCadaClasse)
+        })
+    }
+
+    bindItens() {
+        this.cliqueFora = this.cliqueFora.bind(this);
+        this.adicionarCadaClasse = this.adicionarCadaClasse.bind(this);
+    }
+
+    init() {
+        this.bindItens();
+        this.adicionarClasseEvento();
+        return this;
+    }
+}
