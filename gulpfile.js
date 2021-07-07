da7a26aadc7abb80369102adce3b46add39326eb
Lucas Fernandes <Lucas_201130@hotmail.com>
2021-07-06 23:16:08 -0300

    minyfy

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,14 @@ const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
-const css = require('gulp-uglifycss')
+const css = require('gulp-uglifycss');
+const htmlmin = require('gulp-htmlmin')
+
+function html() {
+    return gulp.src('./dest/index.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./'))
+}
 
 function adicionar() {
     return gulp.src('./style/style.css')
@@ -25,3 +32,4 @@ function arquivo() {
 
 gulp.task('a', adicionar);
 gulp.task('c', arquivo)
+gulp.task('h', html)
